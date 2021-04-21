a37614ed374775562371803217d149cafc122fc3
Leonardo Silva <silvaleonardo.ti@gmail.com>
2021-04-21 19:09:37 -0300

    Adicionando schema para criação da entidade no BD

---
+++ database/sah_bd_schema.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 22/04/2021 às 00:07
+-- Versão do servidor: 10.4.18-MariaDB
+-- Versão do PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `sah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `ajustes`
+--
+
+CREATE TABLE `ajustes` (
+  `id` int(11) NOT NULL,
+  `data` date NOT NULL DEFAULT current_timestamp(),
+  `hora_entrada` time DEFAULT NULL,
+  `hora_saida` time DEFAULT NULL,
+  `justificativa` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `ajustes`
+--
+ALTER TABLE `ajustes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `ajustes`
+--
+ALTER TABLE `ajustes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
